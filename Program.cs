d17ac5ea6a86e977a224f628360575837de2c055
Wajahat Abbas <155806602+DevWajahat@users.noreply.github.com>
2024-10-18 19:50:08 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+namespace c__lecture_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+          Cow cowobj  = new Cow();
+            cowobj.name = "cow";
+            cowobj.legs = 4;
+            cowobj.eye = 2;
+            cowobj.Color = "brown";
+            
+            cowobj.details();
+            Console.WriteLine("It has " + cowobj.legs + " legs " + " and " + cowobj.eye + " eyes");
+
+            dog dobj = new dog();
+            dobj.name = "dog";
+            dobj.legs = 4;
+            dobj.eye = 2;
+            dobj.Color = "black";
+
+            dobj.details();
+            Console.WriteLine("It has " + dobj.legs + " legs " + " and " + dobj.eye + " eyes");
+        }
+    }
+
+    public class Animal
+    {
+        public string name;
+        public string Color;
+        public int eye;
+        public int legs;
+
+    }
+    public class Cow : Animal
+    {
+        public void details()
+        {
+            Console.WriteLine("The name of animal is "+name+" and its color is " + Color);
+        }
+
+
+    }
+    public class dog : Animal { 
+    public void details()
+        {
+            Console.WriteLine("The name of animal is " + name + " and its color is " + Color);
+        }
+    }
+}
